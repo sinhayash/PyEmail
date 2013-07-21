7955f5af0d61551a2eab6cfcaa6b186d41c92f9b
Yash Sinha <mail.yash.sinha@gmail.com>
2013-07-21 10:45:15 +0530

    sendEmail() function created

--- pyemail.py
+++ pyemail.py
@@ -1,5 +1,13 @@
 import smtplib
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
-server.login('<gmail_address>', '<gmail_password>')
-server.sendmail('<from>', 'someone@gmail.com', '<message>')
+
+
+def sendEmail(gmailAddress,
+              gmailPassword,
+              recipientAddress,
+              message,
+              smtpServer="smtp.gmail.com"):
+    server = smtplib.SMTP(smtpServer, 587)
+    server.starttls()
+    server.login(gmailAddress, gmailPassword)
+    server.sendmail(gmailAddress, recipientAddress, message)
+    server.quit()
